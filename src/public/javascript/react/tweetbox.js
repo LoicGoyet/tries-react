ead8b5bdfb4cb894707163cc26048a2ae8fce758
Loïc Goyet <loic@appventus.com>
2016-09-19 12:22:42 +0200

    set constants instead of magic numbers

--- src/public/javascript/react/tweetbox.js
+++ src/public/javascript/react/tweetbox.js
@@ -1,3 +1,6 @@
+const TWEET_MAX_LENGTH = 140;
+const PHOTO_LENGTH = PHOTO_LENGTH;
+
 var TweetBox = React.createClass({
     getInitialState: function() {
         return {
@@ -13,22 +16,24 @@ var TweetBox = React.createClass({
     },
     remainingCharacters: function() {
         if (this.state.photoAdded) {
-            return 140 - 23 - this.state.text.length;
+            return TWEET_MAX_LENGTH - PHOTO_LENGTH - this.state.text.length;
         } else {
-            return 140 - this.state.text.length;
+            return TWEET_MAX_LENGTH - this.state.text.length;
         }
     },
     disableTweetButton: function () {
-        return this.remainingCharacters() === 140 || this.remainingCharacters() < 0;
+        return this.remainingCharacters() === TWEET_MAX_LENGTH || this.remainingCharacters() < 0;
     },
     overflowAlert: function() {
         if (this.remainingCharacters() < 0) {
+            const BEFORE_OVERFLOW_LENGTH = 10;
+
             if (this.state.photoAdded) {
-                var beforeOverflowText = this.state.text.substring(140 - 23 - 10, 140 - 23);
-                var overflowText = this.state.text.substring(140 - 23);
+                var beforeOverflowText = this.state.text.substring(TWEET_MAX_LENGTH - PHOTO_LENGTH - BEFORE_OVERFLOW_LENGTH, TWEET_MAX_LENGTH - PHOTO_LENGTH);
+                var overflowText = this.state.text.substring(TWEET_MAX_LENGTH - PHOTO_LENGTH);
             } else {
-                var beforeOverflowText = this.state.text.substring(140 - 10, 140);
-                var overflowText = this.state.text.substring(140);
+                var beforeOverflowText = this.state.text.substring(TWEET_MAX_LENGTH - BEFORE_OVERFLOW_LENGTH, TWEET_MAX_LENGTH);
+                var overflowText = this.state.text.substring(TWEET_MAX_LENGTH);
             }
 
             return (
